5a77cad4cf85edd076173832a70282e44ff4c3a5
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-04-15 21:19:39 -0300

    Initial commit from Create Next App

---
+++ config/site.ts
@@ -0,0 +1,50 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+    name: "Next.js + NextUI",
+    description:
+        "Make beautiful websites regardless of your design experience.",
+    navItems: [
+        {
+            label: "Home",
+            href: "/",
+        },
+        {
+            label: "Pricing",
+            href: "/pricing",
+        },
+        {
+            label: "Blog",
+            href: "/blog",
+        },
+        {
+            label: "About",
+            href: "/about",
+        },
+    ],
+    navMenuItems: [
+        {
+            label: "Home",
+            href: "/",
+        },
+        {
+            label: "Pricing",
+            href: "/pricing",
+        },
+        {
+            label: "Blog",
+            href: "/blog",
+        },
+        {
+            label: "About",
+            href: "/about",
+        },
+    ],
+    links: {
+        github: "https://github.com/nextui-org/nextui",
+        twitter: "https://twitter.com/getnextui",
+        docs: "https://nextui.org",
+        discord: "https://discord.gg/9b6yyZKmH4",
+        sponsor: "https://patreon.com/jrgarciadev",
+    },
+};
