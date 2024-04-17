8ed000837e56f4aaefd77be856e7da34662a7ffd
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-04-16 22:26:13 -0300

    Añadir cambios en el pie de página de la UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
 						<main className="flex-grow">
 							{children}
 						</main>
-						<footer className="w-full flex items-center justify-center py-3">
+						{/* <footer className="w-full flex items-center justify-center py-3">
 							<Link
 								isExternal
 								className="flex items-center gap-1 text-current"
@@ -57,7 +57,7 @@ export default function RootLayout({
 								<span className="text-default-600">Powered by</span>
 								<p className="text-primary">NextUI</p>
 							</Link>
-						</footer>
+						</footer> */}
 					</div>
 				</Providers>
 			</body>
