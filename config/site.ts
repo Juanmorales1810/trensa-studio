86b6e4c885ace2b17a178918752ee159997cd7b9
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-04-26 21:55:02 -0300

    Actualizar nombre y descripción del sitio en site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,43 +1,26 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-    name: "Next.js + NextUI",
-    description:
-        "Make beautiful websites regardless of your design experience.",
+    name: "Trenza Studio",
+    description: "Fotografía, diseño y desarrollo web. Todo en un solo lugar.",
     navItems: [
-        {
-            label: "Home",
-            href: "/",
-        },
-        {
-            label: "Pricing",
-            href: "/pricing",
-        },
         {
             label: "Blog",
             href: "/blog",
         },
         {
-            label: "About",
-            href: "/about",
+            label: "Portfolio",
+            href: "/portfolio",
         },
     ],
     navMenuItems: [
         {
-            label: "Home",
-            href: "/",
-        },
-        {
-            label: "Pricing",
-            href: "/pricing",
-        },
-        {
             label: "Blog",
             href: "/blog",
         },
         {
-            label: "About",
-            href: "/about",
+            label: "Portfolio",
+            href: "/portfolio",
         },
     ],
     links: {
