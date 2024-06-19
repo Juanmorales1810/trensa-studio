fbc9a42021570a27ff1e7d458c2f967bc21d6f22
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-06-18 21:49:51 -0300

    Fix: Cambios en la UI para movil

--- components/bento.tsx
+++ components/bento.tsx
@@ -46,7 +46,7 @@ export default function Bento() {
                     className={`row-span-1 group rounded-xl border-2 border-slate-100 dark:border-slate-400/10 bg-neutral-900 overflow-hidden hover:shadow-xl transition-shadow ${i === 0 || i === 4 ? "row-span-2" : ""
                         }`}
                 >
-                    <Image src={cont.image} alt={cont.title} width={800} height={400} className="rounded-none object-cover h-full opacity-25 saturate-0 group-hover:opacity-100 group-hover:saturate-100 transition-opacity" />
+                    <Image src={cont.image} alt={cont.title} width={800} height={400} className="rounded-none object-cover h-full md:opacity-25 md:saturate-0 md:group-hover:opacity-100 md:group-hover:saturate-100 md:transition-opacity" />
                 </div>
             ))}
         </div>
