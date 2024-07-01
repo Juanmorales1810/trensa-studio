d016c914f824199198174605bb1f63e6e4ccf099
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-07-01 19:49:34 -0300

    Fix: Children layout

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,7 +4,8 @@ import * as React from "react";
 import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ThemeProviderProps } from "next-themes/dist/types";
+import { ThemeProviderProps } from "@/types";
+
 
 export interface ProvidersProps {
 	children: React.ReactNode;
