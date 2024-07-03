3361040e23264a1c1a90da30386926cbfad23e08
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-07-03 14:32:35 -0300

    Feat: Se creo la base de datos del porfolio

---
+++ config/portfolio.ts
@@ -0,0 +1,124 @@
+interface PortfolioConfig {
+    title: string;
+    location: string;
+    date: string;
+    slug: string;
+    coverImage: string;
+    cardImage: string;
+    porfolioImages: string[];
+    description: string;
+}
+
+export const portfolioDB: PortfolioConfig[] = [
+    {
+        title: "Boda de Vale y Felipe",
+        location: "Casona San Ignacio",
+        date: "26 Diciembre 2023",
+        slug: "vale-y-felipe-casona-san-ignacio",
+        coverImage: "hsdpa8qxihmi5aypoiyj",
+        cardImage: "/portfolio/Casona-San-Ignacio-1066-768x512.webp",
+        porfolioImages: [
+            "pcw3izqynxxoumuy1zd9",
+            "kbzbpqvunurx0sqvdixk",
+            "fecdi6layz73tnbmddmk",
+            "xbmzeh9sj3uj9qbq4m1u",
+            "fbfta5nfefilodpfsirm",
+            "urrw7kptnguwth5oipjq",
+            "ur0sj15qtnwqcbayuxxr",
+            "iyzxkr1z90mzskjygjxf",
+            "hsdpa8qxihmi5aypoiyj",
+            "gkspayy2h5nzybxuisv6",
+            "yyzy9fejqa6lctrailng",
+            "pznf9ucztfrlexy7m43k",
+            "lquvvv3kinewthrvnpt7",
+            "vtkm8imkoxednvyj5pop",
+            "d0mwqkkdaaejorjndlot",
+            "vjedjlboitio7nu3efqc",
+            "hw0r24ugslob9jvfewjt",
+            "xjxefq38icg4qgahnluj",
+            "lvvh7kgpkrb0h0vxapt8",
+            "lzai32vq63r8kb511unl",
+            "oc4dmdmjaokvifcxtkfs",
+            "a9zqbmdeos1vkwobnj0u",
+            "fcgje65ucgqccuu5ilyl",
+            "srf7v2oxxl62wepwlt9q",
+            "ekl6hv6c5kqno71csn82",
+            "k13u1wsuo6mgahx3dx1g",
+            "xkwdde238yysi9rcga1s",
+            "tpnx9aeihi3aw3kzcgv5",
+            "vgwnd0srfqjrewuupown",
+            "fq7v9klutdfr5ih4nus9",
+            "hwsi4zfupbusjcxsudad",
+            "gof4b4dar1tvbclsbqlw",
+            "nynyg7ntqnibzl9yzkhz",
+            "ddzmnbs39ayf6pqnxv5s",
+            "z0culwqrsyl06f8dmi2r",
+            "e41kjz6ywjiqzptq7q03",
+            "e160ptnptismkxas4mcm",
+            "x1gyu7vn2hpingsrmptt",
+            "rh3s1d57sbllzxajrfec",
+            "ml81uvho7vngugeqsbqx",
+            "gn2jzxzikprvzpevvfwi",
+            "iarkai9bvlobvc08gdnw",
+            "gd6sbkveblt57llfig0u",
+            "dpaopzpkgyohmfdhamja",
+            "eadvls9mkz1efledprgi",
+            "ibsburhd7lhmdkuedjig",
+            "qsqok0e7opqgwhrwmetv",
+            "eqh6ahkrfuttelxzonx4",
+            "mv0ovo4qlprraga3edwk",
+            "jm6pycngcm9j5c9u5gso",
+            "hm8nlspl21zyjkxaepno",
+            "yqn9llbevkxesguxlb6a",
+            "u3t1airvnlxju280motu",
+            "x5gl6d3phjimxssukhrg",
+            "wxbjhxrwzmlbpskhkrgo",
+            "ed6qxnndwttbtp0smdmd",
+            "nycrerxtnw3ys5ligur4",
+            "zuufh33tm9sdwy4uybzx",
+            "vds4v3mje0hmjrpsqkiv",
+            "vuw1fgbpyyhvdknbb5wm",
+            "w8afsyp6dkzev4d7d3ib",
+            "rmq6tyxmtccrhjpafqfd",
+            "awo2e4wqjixcfovgj8og",
+            "oww23srykfxw1rzutnjh",
+            "gjyhypgug15m5rt96exd",
+            "nzins0wapbbacpj48qmu",
+            "r2mexsl57jdlbwnnirc0",
+            "mxopjdjuf1fujmgwenpm",
+            "rt51g5efcxhnwqmcfmez",
+            "hpn4wrjotra9pm5ucobu",
+            "avb62gxlq3r1umcrhcxl",
+            "ccou6smoj5xm8iguzdlu",
+            "zmsjqlxnmyorszxvooqm",
+            "gwoiqgxvfftrbukkozbi",
+            "r7ysdsmiavvgwhrc2r9o",
+            "yrcaajzifowrbap8iedp",
+            "xribddkvvpledzppca6v",
+            "dqpukbm1sqtvedhq5xpy",
+        ],
+        description: `
+### Matrimonio en Casona San Ignacio –
+
+Partimos la preparación de la Vale en la casa de sus papás, era un día lluvioso, había bastante frio, pero no había clima que pudiera con lo que se venia gestando ya! desde la preparación.
+La Vale estaba con todo su familia, su hermana fue su maquilladora, si esta mujer es hermosa de por si, la Maca le resaltó toda esa belleza que tiene!
+Otra cosa muy linda es que la Mamá de la Vale fue quien el hizo el vestido. Ella se inspiró lo diseño y su mamá lo confeccionó junto a varios otros accesorios, para ese día.
+
+### La Iglesia
+
+Nos fuimos a la Iglesia, allá estaba Felipe junto a su familia recibiendo a los invitados.
+Además tuvimos una suerte gigante, para la entrada de la Vale lloviznaba levemente y cuando les tocó hacer la salida triunfal NO estaba lloviendo!
+Así que fue perfecto! los invitados pudieron usar sus plumeros de colores para la Vale y Felipe salieron De la Iglesia.
+
+### Casona San Ignacio
+
+Y luego de ceremonia religiosa nos fuimos a Casona San Ignacio.
+Como estaba frio y había poca luz, aprovechamos cada rincón de la Casona San Ignacio para hacer la sesión de novios, que según nosotros quedó soñada!
+Como verán en las fotos, la fiesta estuvo atómica, los novios mas bailarines de la temporada! Se pegaron un baile de Novios de otro nivel, la coreografía de Dirty Dancing! no erraron ni un solo paso! unos genios! Ahhhh! olvidé decir que llego la [Vivi Rodrigues](https://www.instagram.com/vivi.lyh/) para ponerlos a bailar Axe a todos en la pista!
+
+**Makeup y Peinado:** [Maca Leichtle](https://www.instagram.com/macaleichtle/?hl=es)
+**Vestido:** La Mamá <3
+**Lugar:** [Casona San Ignacio](https://www.casonasanignacio.com/)
+        `,
+    },
+];
