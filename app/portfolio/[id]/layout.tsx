4f085021451a26df275014203451256bc1ceae0f
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-07-04 09:19:28 -0300

    Fix: Se mejoro la semántica del openGraph

--- app/portfolio/[id]/layout.tsx
+++ app/portfolio/[id]/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, ResolvingMetadata } from 'next'
 import { portfolioDB } from "@/config/portfolio";
+import { siteConfig } from '@/config/site';
 
 type Props = {
     params: { id: string }
@@ -19,7 +20,26 @@ export async function generateMetadata(
         title: foundPorfolio?.title,
         description: foundPorfolio?.location,
         openGraph: {
-            images: foundPorfolio?.coverImage,
+            title: foundPorfolio?.title,
+            description: foundPorfolio?.location,
+            url: 'https://www.trenzaestudiofotografia.cl/',
+            siteName: siteConfig.name,
+            images: [
+                {
+                    url: `https://res.cloudinary.com/dli8ejcjv/image/upload/v1719876222/${foundPorfolio?.slug}/${foundPorfolio?.coverImage}.webp`,
+                    width: 1200,
+                    height: 630,
+                },
+
+            ],
+            locale: 'es_CL',
+            type: 'website',
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title: foundPorfolio?.title,
+            description: foundPorfolio?.location,
+            images: [`https://res.cloudinary.com/dli8ejcjv/image/upload/v1719876222/${foundPorfolio?.slug}/${foundPorfolio?.coverImage}.webp`],
         },
     }
 }
