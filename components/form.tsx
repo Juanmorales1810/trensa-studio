1040cb97d84b5ff9dc7a6707d0228db5deb5499f
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-10-01 17:40:03 -0300

    Add form component with input fields, date picker, textarea, checkboxes, and submit button

---
+++ components/form.tsx
@@ -0,0 +1,24 @@
+import { DatePicker } from "@nextui-org/date-picker";
+import { Input, Textarea } from "@nextui-org/input";
+import { Checkbox } from "@nextui-org/checkbox";
+import { Button } from "@nextui-org/button";
+
+export default function Form() {
+    return (
+        <form className="flex flex-col gap-2 md:w-1/2">
+            <Input color="primary" label="Nombre completo" />
+            <Input color="primary" label="Teléfono" />
+            <Input color="primary" label="Mail" />
+            <DatePicker color="primary" label="Fecha del evento" />
+            <Textarea color="primary" label="Tienes alguna consulta?" />
+            <p className="text-lg font-bold">¿Cómo prefieres que nos contactemos?</p>
+            <Checkbox color="primary">
+                Teléfono
+            </Checkbox>
+            <Checkbox color="primary">
+                WhatsApp
+            </Checkbox>
+            <Button type="submit" className="bg-[#7CA4DC] text-black font-semibold">Enviar</Button>
+        </form>
+    )
+}
