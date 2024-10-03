516b8d39d3b5f4c289d9eff5e0195a5b9e3207eb
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-10-02 21:01:57 -0300

    Fix: Se corrigió el la animación

--- components/ui/parallax-scroll.tsx
+++ components/ui/parallax-scroll.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { useScroll, useTransform } from "framer-motion";
-import { useRef } from "react";
-import { motion } from "framer-motion";
-import Image from "next/image";
-import { cn } from "@/utils/cn";
 import { Button } from "@nextui-org/button";
+import { motion } from "framer-motion";
+import { cn } from "@/utils/cn";
+import { useRef } from "react";
+import Image from "next/image";
 import Link from "next/link";
 interface PortfolioConfig {
     title: string;
@@ -26,8 +26,7 @@ export const ParallaxScroll = ({
 }) => {
     const gridRef = useRef<any>(null);
     const { scrollYProgress } = useScroll({
-        container: gridRef, // remove this if your container is not fixed height
-        offset: ["start start", "end start"], // remove this if your container is not fixed height
+
     });
 
     const translateFirst = useTransform(scrollYProgress, [0, 1], [0, -200]);
@@ -42,11 +41,11 @@ export const ParallaxScroll = ({
 
     return (
         <div
-            className={cn("h-[calc(100vh-128px)] items-start overflow-y-auto w-full no-scrollbar", className)}
+            className={cn("h-[calc(100vh-128px)] items-start overflow-y-auto w-full no-scrollbar md:pb-40 md:h-auto", className)}
             ref={gridRef}
         >
             <div
-                className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-start  max-w-5xl mx-auto md:gap-10 md:py-10 px-10"
+                className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-start  max-w-5xl mx-auto gap-10 py-10 px-10"
                 ref={gridRef}
             >
                 <div className="grid gap-10">
