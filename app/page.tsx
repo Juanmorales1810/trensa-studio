5ce69b39c187c9a8ce4e846b174131aa9cab2ec6
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-10-05 19:17:23 -0300

    Fix: Cambios en la UI

--- app/page.tsx
+++ app/page.tsx
@@ -57,11 +57,11 @@ export default function Home() {
 	];
 	return (
 		<section className="flex flex-col w-full items-center overflow-hidden md:overflow-visible">
-			<section className="relative h-auto min-h-screen w-full">
-				<div className="h-auto w-full [mask-image:radial-gradient(ellipse_80%_100%_at_100%_-10%,#000_90%,transparent_110%)]">
+			<section className="relative h-auto min-h-screen w-full mt-16">
+				<div className="h-auto w-full [mask-image:radial-gradient(ellipse_90%_100%_at_100%_-10%,#000_90%,transparent_110%)]">
 					<SwiperHome />
 				</div>
-				<div className="flex flex-col justify-center items-center z-30 absolute bottom-10 left-[0%] md:left-[15%] md:top-1/2">
+				<div className="flex flex-col justify-center items-center z-30 absolute bottom-16 left-[0%] md:left-[32%] md:top-1/2">
 					<Image src="/logo/Logo sin Nada.png" alt="Logo" width={400} height={124} className="pl-4 mb-4 rounded-none w-80 md:w-full" />
 					<h1 className="text-3xl font-bold text-center mb-1 w-full max-w-2xl hidden md:block">Fotografía y Video Profesional para Bodas y Matrimonios en La Serena y Santiago</h1>
 					<p className="text-lg text-center text-zinc-700 w-full max-w-2xl hidden md:block">
@@ -75,31 +75,31 @@ export default function Home() {
 			</section>
 			<section id="services" data-section="services" className="flex flex-col justify-center items-center z-10 w-full pt-16 px-2">
 				<h2 className="text-2xl font-semibold text-center">Servicios Principales Fotografía Matrimonios</h2>
-				<p className="text-lg text-center text-zinc-700 w-full max-w-2xl">
-					Preservamos los momentos más bellos de tu día especial. Ofrecemos sesiones fotográficas
-					en un estilo que se ajusta a tu visión: documental, clásico o artístico.
-				</p>
-				<section className="flex flex-wrap max-w-5xl w-full justify-center items-center gap-2 py-10 h-auto md:justify-between md:gap-8">
-					<article className="relative w-full rounded-xl group md:w-[300px] md:h-[380px]">
-						<div className="absolute inset-0 z-20 flex justify-center items-center flex-col py-14 px-6">
-							<h3 className="text-3xl font-bold text-zinc-50 select-none text-center flex-1">Registro completo de la boda</h3>
-							<Button as={Link} href="/services" className="bg-[#7CA4DC] w-28 text-black font-semibold">Ver más</Button>
+				<section className="flex flex-col max-w-5xl w-full justify-center items-center gap-2 py-10 h-auto md:justify-between md:gap-8 md:flex-row">
+					<article className="relative w-full h-80 rounded-xl group overflow-hidden md:max-w-[300px] md:h-[480px] bg-zinc-950">
+						<div className="absolute inset-0 z-20 flex justify-center items-center flex-col py-6 px-4">
+							<h3 className="text-3xl font-bold text-zinc-50 select-none text-center flex-1">Fotografía de Bodas en La Serena y Santiago</h3>
+							<p className="pb-10 text-pretty text-center text-zinc-50">Preservamos los momentos más bellos de tu día especial. Ofrecemos sesiones fotográficas
+								en un estilo que se ajusta a tu visión: documental, clásico o artístico.</p>
+							<Button as={Link} href="/servicios/fotografia-bodas-en-la-serena-y-santiago" className="bg-[#7CA4DC] w-28 text-black font-semibold">Ver más</Button>
 						</div>
-						<Image src="/home/service/service-1.webp" alt="Novios en sesión fotográfica de boda con fondo montañoso en la playa de La Serena." className="object-cover object-top w-[500px] h-[380px] brightness-[0.2] group-hover:scale-105 md:w-[300px] md:h-[380px]" />
+						<img src="/home/service/service-1.webp" alt="Novios en sesión fotográfica de boda con fondo montañoso en la playa de La Serena." className="object-cover object-top w-full [mask-image:radial-gradient(ellipse_140%_95%_at_100%_-10%,#000_10%,transparent_110%)]" />
 					</article>
-					<article className="relative w-full rounded-xl group md:w-[300px] md:h-[380px]">
-						<div className="absolute inset-0 z-20 flex justify-center items-center flex-col py-14 px-6">
-							<h3 className="text-3xl font-bold text-zinc-50 select-none text-center flex-1">Sesión de fotos con novios e invitados</h3>
-							<Button as={Link} href="/services" className="bg-[#7CA4DC] w-28 text-black font-semibold">Ver más</Button>
+					<article className="relative w-full h-80 rounded-xl group overflow-hidden md:max-w-[300px] md:h-[480px] bg-zinc-950">
+						<div className="absolute inset-0 z-20 flex justify-center items-center flex-col py-6 px-4">
+							<h3 className="text-3xl font-bold text-zinc-50 select-none text-center flex-1">Video de Boda con Drone</h3>
+							<p className="pb-10 text-pretty text-center text-zinc-50">Captura cada momento desde perspectivas únicas con nuestro servicio de video profesional. Documentamos toda la ceremonia y la fiesta, con imágenes de alta calidad y drones para tomas aéreas impresionantes.</p>
+							<Button as={Link} href="/servicios/video-de-boda-con-drone" className="bg-[#7CA4DC] w-28 text-black font-semibold">Ver más</Button>
 						</div>
-						<Image src="/home/service/service-2.webp" alt="Fotógrafo de bodas en Santiago capturando el beso de los novios." className="object-cover object-top w-[500px] h-[380px] brightness-[0.2] group-hover:scale-105 md:w-[300px] md:h-[380px]" />
+						<img src="/home/service/service-2.webp" alt="Fotógrafo de bodas en Santiago capturando el beso de los novios." className="object-cover object-top w-full [mask-image:radial-gradient(ellipse_140%_95%_at_100%_-10%,#000_10%,transparent_110%)]" />
 					</article>
-					<article className="relative w-full rounded-xl group md:w-[300px] md:h-[380px]">
-						<div className="absolute inset-0 z-20 flex justify-center items-center flex-col py-14 px-6">
-							<h3 className="text-3xl font-bold text-zinc-50 select-none text-center flex-1">Fotografía de preparativos y ceremonia</h3>
-							<Button as={Link} href="/services" className="bg-[#7CA4DC] w-28 text-black font-semibold">Ver más</Button>
+					<article className="relative w-full h-80 rounded-xl group overflow-hidden md:max-w-[300px] md:h-[480px] bg-zinc-950">
+						<div className="absolute inset-0 z-20 flex justify-center items-center flex-col py-6 px-4">
+							<h3 className="text-3xl font-bold text-zinc-50 select-none text-center flex-1">Cobertura Completa de tu Boda</h3>
+							<p className="pb-10 text-pretty text-center text-zinc-50">Documentamos desde los preparativos hasta la recepción. Aseguramos que cada emoción, detalle y momento sea capturado con nuestra cobertura completa de fotografía y video.</p>
+							<Button as={Link} href="/servicios/cobertura-completa-de-tu-boda" className="bg-[#7CA4DC] w-28 text-black font-semibold">Ver más</Button>
 						</div>
-						<Image src="/home/service/service-3.webp" alt="Decoración floral en boda en La Serena, fotografía creativa de matrimonio." className="object-cover object-top w-[500px] h-[380px] brightness-[0.2] group-hover:scale-105 md:w-[300px] md:h-[380px]" />
+						<img src="/home/service/service-3.webp" alt="Decoración floral en boda en La Serena, fotografía creativa de matrimonio." className="object-cover object-top w-full [mask-image:radial-gradient(ellipse_140%_95%_at_100%_-10%,#000_10%,transparent_110%)]" />
 					</article>
 				</section>
 			</section>
@@ -141,9 +141,23 @@ export default function Home() {
 			</section>
 			<section className="grid items-center justify-center grid-cols-1 px-4 py-24 text-center lg:grid-cols-3">
 				<div className="col-auto lg:col-start-2">
-					<p className="mb-2 text-3xl font-semibold text-primary">Descarga nuestra guía completa de servicios</p>
-					<h2 className="mb-6 text-2xl font-normal md:leading-tight md:mb-6">y conoce todos los detalles sobre cómo podemos capturar tu boda de una manera única y especial.</h2>
-					<Button as={Link} href="/Guia de Consultas Fotografía y Video Trenza Estudio.pdf" target="_blank" className="bg-[#7CA4DC] mt-4 font-semibold text-black">Descargar curso</Button>
+					<p className="mb-2 text-3xl font-bold text-primary md:text-nowrap">Descarga nuestra guía completa de servicios</p>
+					<h2 className="mb-6 text-2xl font-normal md:leading-tight md:mb-6 px-2">y conoce todos los detalles sobre cómo podemos capturar tu boda de una manera única y especial.</h2>
+					<Button
+						as={Link}
+						href="/Guia de Consultas Fotografía y Video Trenza Estudio.pdf"
+						target="_blank"
+						className="bg-[#7CA4DC] mt-4 font-semibold text-black"
+						endContent={
+							<svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path
+									fillRule="evenodd"
+									d="M6.672 1.911a1 1 0 10-1.932.518l.259.966a1 1 0 001.932-.518l-.26-.966zM2.429 4.74a1 1 0 10-.517 1.932l.966.259a1 1 0 00.517-1.932l-.966-.26zm8.814-.569a1 1 0 00-1.415-1.414l-.707.707a1 1 0 101.415 1.415l.707-.708zm-7.071 7.072l.707-.707A1 1 0 003.465 9.12l-.708.707a1 1 0 001.415 1.415zm3.2-5.171a1 1 0 00-1.3 1.3l4 10a1 1 0 001.823.075l1.38-2.759 3.018 3.02a1 1 0 001.414-1.415l-3.019-3.02 2.76-1.379a1 1 0 00-.076-1.822l-10-4z"
+									clipRule="evenodd"
+								/>
+							</svg>
+						}
+					>Descargar curso</Button>
 				</div>
 			</section>
 		</section>
