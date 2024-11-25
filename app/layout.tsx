c0887f08b8943e48c9d02c1c3ea592469324fda2
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-11-25 12:23:32 -0300

    Feat: Agregar soporte para Google Analytics y actualizar dependencias de Next.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Providers } from "./providers";
 import { Navbar } from "@/components/navbar";
 import clsx from "clsx";
 import Footer from "@/components/footer";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import GoogleAnalytics from "@/components/googleAnalytics";
 
 export const viewport: Viewport = {
 	themeColor: [
@@ -124,6 +124,7 @@ export default function RootLayout({
 					fontSans.variable
 				)}
 			>
+				<GoogleAnalytics />
 				<Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
 					<div className="relative flex flex-col">
 						<div className="absolute h-full w-full bg-[radial-gradient(#bbbbbb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)] dark:bg-[radial-gradient(#1b1b1b_1px,transparent_1px)] z-0"></div>
@@ -135,7 +136,6 @@ export default function RootLayout({
 					</div>
 				</Providers>
 			</body>
-			<GoogleAnalytics gaId="G-2R3T93JW6H" />
 		</html>
 	);
 }
