4afbd3d613c5397b21f136e07647091111fd62ca
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-11-25 21:05:33 -0300

    Feat: Implementar carga de blogs desde MongoDB y mejorar el diseño de la página

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,9 +1,34 @@
-import { title } from "@/components/primitives";
+import CardBlog from "@/components/cardBlogs";
+import { connectMongoDB } from "@/lib/mongodb";
+import { TrenzaMatrimoniosBlogsModel } from "@/models/trenza";
+import { cache } from "react";
 
-export default function BlogPage() {
+const getItems = cache(async function loadMenu() {
+	await connectMongoDB();
+	const ListBlogs = await TrenzaMatrimoniosBlogsModel.find();
+	return ListBlogs.map(blogs => {
+		const obj = blogs.toObject();
+		obj._id = obj._id.toString(); // Convierte _id a una cadena
+		return obj;
+	}); // Usa .toObject() para convertir cada producto a un objeto JavaScript simple
+})
+export default async function BlogPage() {
+	const blogs = await getItems();
 	return (
 		<div>
-			<h1 className={title()}>Blog</h1>
+			<h1 className="h-16 text-3xl font-semibold">Blogs</h1>
+			<div className="grid grid-cols-1 gap-4 w-full max-w-6xl mx-auto md:grid-cols-2">
+				{blogs.map((blog) => (
+					<CardBlog
+						key={blog._id}
+						title={blog.title}
+						description={blog.description}
+						image={blog.image}
+						date={blog.createdAt}
+						slug={blog?.slug || blog._id}
+					/>
+				))}
+			</div>
 		</div>
 	);
 }
